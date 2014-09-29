9c8c459f9db0a292bba1671f6a2e0b3832f7b2d5
Jan Andersson <jan.andersson@gmail.com>
2014-09-29 13:37:45 +0200

    Add Component#name

--- lib/electronics/component.rb
+++ lib/electronics/component.rb
@@ -1,6 +1,9 @@
 module Electronics
   class Component
-
+    attr_reader :name
+    def initialize(opts = {})
+      @name = opts.fetch(:name, "")
+    end
   end
 end
 
--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 describe Electronics::Component do
   let(:component) { described_class.new }
 
+  describe "#name" do
+    let(:component_with_name) { described_class.new(name: 'foo') }
+    it "has no name by default" do
+      expect(component.name).to be_empty
+    end
+
+    it "can have a name" do
+      expect(component_with_name.name).to eq 'foo'
+    end
+  end
 end
