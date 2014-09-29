1e542e6131f46069ce37fb7dc576e0ddc0fd47f0
Jan Andersson <jan.andersson@gmail.com>
2014-09-29 15:56:51 +0200

    Return zero voltage if none set

--- lib/electronics/pin.rb
+++ lib/electronics/pin.rb
@@ -1,9 +1,13 @@
 module Electronics
   class Pin
-    attr_reader :name, :voltage
+    attr_reader :name
     def initialize(opts = {})
       @name = opts.fetch(:name, "")
       @voltage = opts.fetch(:voltage, nil)
+    end
+
+    def voltage
+      @voltage || 0
     end
   end
 end
--- spec/pin_spec.rb
+++ spec/pin_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Electronics::Pin do
   let(:pin) { described_class.new }
-  it "has blank name and voltage by default" do
+  it "has blank name and zero voltage by default" do
     expect(pin.name).to eq ""
-    expect(pin.voltage).to eq nil
+    expect(pin.voltage).to eq 0
   end
 end
