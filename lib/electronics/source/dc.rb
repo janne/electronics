3f9f27d31469638a6080daa2a363521370228727
Jan Andersson <jan.andersson@gmail.com>
2014-09-29 13:56:19 +0200

    Add Source::DC#voltage

--- lib/electronics/source/dc.rb
+++ lib/electronics/source/dc.rb
@@ -1,7 +1,9 @@
 module Electronics
   module Source
     class DC < Component
+      attr_reader :voltage
       def initialize(opts = {})
+        @voltage = opts.fetch(:voltage, 9)
         super(opts)
       end
     end
--- spec/source/dc_spec.rb
+++ spec/source/dc_spec.rb
@@ -7,4 +7,13 @@ describe Electronics::Source::DC do
     dc_with_name = described_class.new(name: "+5V")
     expect(dc_with_name.name).to eq "+5V"
   end
+
+  it "has 9V by default" do
+    expect(dc.voltage).to eq 9
+  end
+
+  it "allows setting voltage" do
+    dc_with_5v = described_class.new(voltage: 5)
+    expect(dc_with_5v.voltage).to eq 5
+  end
 end
